ae8bfd353d495dec62ab2f8d85fa73de25fe2f76
saeedSAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2024-02-21 20:52:58 +0200

    Update browser_wrapper.py

--- infra/browser_wrapper.py
+++ infra/browser_wrapper.py
@@ -1,3 +1,5 @@
+import json
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -5,11 +7,19 @@ from selenium.webdriver.edge.service import Service as EdgeService
 
 
 class BrowserWrapper:
-    def __init__(self, browser_name='chrome'):
-        self.browser_name = browser_name
+    try:
+        with open('../tests/config.json') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("Error: 'config.json' file not found. Make sure the file exists in the correct location.")
+        raise  # Raise the error to halt execution if the file is essential for the script to run
+
+    def __init__(self):
+        self.browser_name = None
         self.driver = None
 
     def start_browser(self):
+        self.browser_name = data.get("browser_name")
         if self.browser_name.lower() == 'chrome':
             self.driver = webdriver.Chrome(service=ChromeService(executable_path="/path/to/chromedriver"))
         elif self.browser_name.lower() == 'firefox':
@@ -22,9 +32,10 @@ class BrowserWrapper:
     def close_browser(self):
         if self.driver:
             self.driver.quit()
-            self.driver = None
 
     def get_driver(self):
-        if not self.driver:
+        if data.get("grid"):
             self.start_browser()
         return self.driver
+
+
